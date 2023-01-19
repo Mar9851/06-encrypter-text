844d17ecc6fac1b4ec9f58419a9efbd0c38874ad
marlon <m.j.moreno.developer@gmail.com>
2023-01-19 02:02:45 -0300

    mejorando el css y codigo script

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
-let btnEncrypt = document.querySelector(".btn-encrypt");
-let btnDecrypt = document.querySelector(".btn-decrypt");
-let btnCopy = document.querySelector(".btn-copy");
-let leftArea = document.querySelector('.left-textarea');
+
+const expresionPositiva = /^[a-z\s?]+$/;
+
+
+
+let btnEncrypt = document.getElementById("btn-encrypt");
+let btnDecrypt = document.getElementById("btn-decrypt");
+let btnCopy = document.getElementById("btn-copy");
+let textArea1 = document.getElementById('textarea1');
 let rightArea = document.querySelector('.right-textarea');
 
 btnEncrypt.addEventListener('click',encrypt);
-btnDecrypt.addEventListener('click',decrypt)
-btnCopy.addEventListener('click',copyText)
+btnDecrypt.addEventListener('click',decrypt);
+btnCopy.addEventListener('click',copyText);
 
 
 
+/**bloque de validacion de texto y estilos de validacion */
+
+const validarTextarea = (e) => {
+   if(expresionPositiva.test(e.target.value)){
+    // desactivo el modo incorrecto de bordes e icono y activo modo valido
+    document.getElementById('area1').classList.remove('validation-icono-bordes-incorrecto');
+    document.getElementById('area1').classList.add('validation-icono-bordes-valido');
+    // desactivo texto incorrecto y activo correcto
+    document.getElementById('text-1').classList.add('text-correcto');
+    document.getElementById('text-1').classList.remove('text-incorrecto');
+    // cambio de icono incorrecto a correcto
+    document.querySelector('#area1 i').classList.add('fa-circle-check');
+    document.querySelector('#area1 i').classList.remove('fa-circle-xmark');
+    // vuelven los botones a su estado inicial
+    document.getElementById('buttons').style.visibility = 'initial';
+
+   }else{
+    // escondo los botones
+    document.getElementById('buttons').style.visibility = 'hidden';
+    // le coloco el incono de incorrecto
+    document.querySelector('#area1 i').classList.add('fa-circle-xmark');
+    // coloco estilos a los bordes y el icono cuando es incorrecto
+    document.getElementById('area1').classList.add('validation-icono-bordes-incorrecto');
+    // desactivo el texto correcto y activo el incorrecto
+    document.getElementById('text-1').classList.remove('text-correcto');
+    document.getElementById('text-1').classList.add('text-incorrecto');
+   }
+}
+
+textArea1.addEventListener('keyup', validarTextarea);
+textArea1.addEventListener('blur', validarTextarea);
+
+btnEncrypt.addEventListener('button', (e) => {
+  e.preventDefault();
+})
+/************************************************************************************************ */
+
+
+
+
+/**bloque de funcion encriptar desencriptar y copiar */
 function encrypt() {
-  var text = leftArea.value.toLowerCase();
+  var text = textArea1.value.toLowerCase();
 
   
   
@@ -20,7 +66,7 @@ function encrypt() {
   var textEncrypt = textEncrypt.replace(/i/gim, "imes");
   var textEncrypt = textEncrypt.replace(/a/gim, "ai");
   var textEncrypt = textEncrypt.replace(/u/gim, "ufat");
-  leftArea.value = '';
+  textArea1.value = '';
   
   document.getElementById("image").style.visibility = "hidden";
   document.querySelector(".right-textarea").innerHTML = textEncrypt;
@@ -30,14 +76,14 @@ function encrypt() {
 
 
 function decrypt() {
-  var text = leftArea.value.toLowerCase();
+  var text = textArea1.value.toLowerCase();
 
   var textEncrypt = text.replace(/enter/gim, "e");
   var textEncrypt = textEncrypt.replace(/ober/gim, "o");
   var textEncrypt = textEncrypt.replace(/imes/gim, "i");
   var textEncrypt = textEncrypt.replace(/ai/gim, "a");
   var textEncrypt = textEncrypt.replace(/ufat/gim, "u");
-  leftArea.value = '';
+  textArea1.value = '';
 
   document.getElementById("image").style.visibility = "hidden";
   document.querySelector(".right-textarea").innerHTML = textEncrypt;
@@ -52,6 +98,8 @@ function copyText() {
   document.getElementById('image').style.visibility = "initial";
   
 }
+
+/********************************************************************************************** */
 
 
 
